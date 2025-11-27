58b2289f29996169866ae54149b79e1b83690f77
ssbg04 <143066036+ssbg04@users.noreply.github.com>
2025-11-27 10:37:21 +0800

    Update HelloWorld.jsx

--- src/pages/HelloWorld.jsx
+++ src/pages/HelloWorld.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 export const HelloWorld = () => {
     return (
-        <div><h1>Hello World!</h1></div>
+        <div>
+            <h1>Team Name: BusyBug</h1>
+            <ul>
+               <li>Marquez, Paulo</li>
+               <li>Garcia, Cris Charles</li>
+               <li>De Vera, Ermhar</li>
+               <li>Patriarca, Rachelle</li>
+               <li>Antonio, Clara Maris</li>
+               <li>Tenorio, Paula Eunice</li>
+            </ul>
+</div>
     );
 };
 
